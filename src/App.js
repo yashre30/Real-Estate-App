277d84f75025df319a52932e66f6716eb63baa2f
yashre30 <kavisujatakavi@gmail.com>
2024-03-24 23:27:12 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Companies from "./Components/Companies/Companies";
+import Contact from "./Components/Contact/Contact";
+import Extra from "./Components/Extra/Extra";
+import Footer from "./Components/Footer/Footer";
+import GetStarted from "./Components/GetStarted/GetStarted";
+import Header from "./Components/Header/Header";
+import Residencies from "./Components/Residencies/Residencies";
+import Value from "./Components/Value/Value";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Header />
+        <Extra />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
     </div>
   );
 }
